d2bd59a23f7d98f68cff3348beceae2fac980066
vison.cao <vison.cao@huolala.cn>
2018-07-22 01:29:04 +0800

    mysql 使用

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,19 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @RequestMapping(value = "/",produces = "text/plain;charset=UTF-8")
-    String index(){
-        return "Hello Spring Boot!";
-    }
 }
